c4b1162b06c99cbd2501ca7a9a80eca00d9f7238
kowah123 <kowah10@gmail.com>
2021-02-10 16:49:04 +1100

    ok

--- Top1B/algorithms.py
+++ Top1B/algorithms.py
@@ -3,4 +3,23 @@ date = input ('date')
 print(name)
 print(date)
 
-input 
+word = input ("word")
+loop 2 times
+  print("word")
+  print("$$$$")
+  
+
+
+number1 = input("number")  
+number2 = input("number") 
+
+result1 = number 1 + number2
+
+print("added result is" + result1)
+
+
+result20 = number2 + 20
+result10 = number + 10
+print (result 20 + 10)
+finalResult = result10 + result20
+print (finalResult)
